68ff2483836486443456fcdfa31aa59ba8016c3c
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-05 12:48:29 +0300

    Indentatie

--- src/bots/mircinator1.ts
+++ src/bots/mircinator1.ts
@@ -1,8 +1,8 @@
-import { Gamestate, BotSelection } from '../models/gamestate';
+import {Gamestate, BotSelection} from '../models/gamestate';
 
 class Bot {
 
-    readonly possibleMoves: BotSelection[] = [ 'R' , 'P' , 'S' , 'D' , 'W'];
+    readonly possibleMoves: BotSelection[] = ['R', 'P', 'S', 'D', 'W'];
 
     dynamitesLeft = 100;
 
@@ -10,33 +10,33 @@ class Bot {
 
         let chosenMove: BotSelection;
 
-        if(gamestate.rounds.length > 0) {
+        if (gamestate.rounds.length > 0) {
             chosenMove = this.getBeatingMove(this.getRandomElement(this.possibleMoves.filter(
                 move => {
-                    if(this.dynamitesLeft > 0) {
+                    if (this.dynamitesLeft > 0) {
                         return move !== this.getOpponentLastMove(gamestate);
-                    }else{
+                    } else {
                         return move !== this.getOpponentLastMove(gamestate) && move !== 'D';
                     }
                 }
             )));
-        }else{
+        } else {
             chosenMove = 'D';
         }
 
-        if(chosenMove === 'D'){
-            if(this.dynamitesLeft < 1)return 'S';
+        if (chosenMove === 'D') {
+            if (this.dynamitesLeft < 1) return 'S';
             this.dynamitesLeft--;
         }
         return chosenMove;
     }
 
-    private getOpponentLastMove(gamestate: Gamestate): BotSelection{
+    private getOpponentLastMove(gamestate: Gamestate): BotSelection {
         return gamestate.rounds[gamestate.rounds.length - 1].p2;
     }
 
     private getBeatingMove(move: BotSelection): BotSelection {
-        switch (move){
+        switch (move) {
             case 'R':
                 return 'P';
             case 'P':
@@ -46,11 +46,11 @@ class Bot {
             case 'D':
                 return 'W';
             case 'W':
-                return this.getRandomElement(['R','P','S']);
+                return this.getRandomElement(['R', 'P', 'S']);
         }
     }
 
-    private getRandomElement(set: BotSelection[]): BotSelection{
+    private getRandomElement(set: BotSelection[]): BotSelection {
         const index = Math.floor(Math.random() * set.length);
         return set[index];
     }

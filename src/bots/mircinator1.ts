a151b5a28442a457abf10137b5f171bd76f2d452
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-05 15:35:08 +0300

    Mircinator V3 Invata stategia de dat apa dupa dinamite consecutive a inamicului

--- src/bots/mircinator1.ts
+++ src/bots/mircinator1.ts
@@ -8,21 +8,54 @@ class Bot {
 
     dynamiteInterval: number;
 
+    defaultConsecTimeBeforeWater = 3;
+
+    opponentConsecutiveDynamiteBeforeWater: number[] = [];
+
     makeMove(gamestate: Gamestate): BotSelection {
 
-        this.dynamiteInterval = this.getRandomNumber(25);
+        this.dynamiteInterval = this.getRandomNumber(15) + 10;
 
         let chosenMove: BotSelection;
 
-        if (gamestate.rounds.length % this.dynamiteInterval === 0) {
-            chosenMove = 'D';
-            this.dynamiteInterval = this.getRandomNumber(25);
+        if (gamestate.rounds.length > 2){
+            if(gamestate.rounds[gamestate.rounds.length - 1].p2 === 'W'){
+                if(gamestate.rounds[gamestate.rounds.length - 2].p2 === 'D'){
+                    this.opponentConsecutiveDynamiteBeforeWater.push(this.numberOfPreviousConsecutiveDynamitedRounds(gamestate, 1));
+                    let matching: number = 0;
+                    for (let i = 0; i < this.opponentConsecutiveDynamiteBeforeWater.length - 1; i++)
+                    {
+                        for (let j = i + 1; j < this.opponentConsecutiveDynamiteBeforeWater.length;j++)
+                        {
+                            if(this.opponentConsecutiveDynamiteBeforeWater[i] === this.opponentConsecutiveDynamiteBeforeWater[j]){
+                                matching++;
+                            }
+                        }
+                    }
+                    if(matching > 3){
+                        this.defaultConsecTimeBeforeWater = this.opponentConsecutiveDynamiteBeforeWater[0];
+                    }
+                }
+            }
+        }
+
+        if ((gamestate.rounds.length % this.dynamiteInterval === 0 || this.hasDrawnLastNRounds(gamestate, 1)) && gamestate.rounds.length > 1) {
+            if (this.numberOfPreviousConsecutiveDynamitedRounds(gamestate, 0) == this.defaultConsecTimeBeforeWater){
+                chosenMove = 'S';
+            }else{
+                chosenMove = 'D';
+                this.dynamiteInterval = this.getRandomNumber(15) + 10;
+            }
         }else{
             if (this.opponentHasRepeatedInTheLastNTurns(gamestate, 5)) {
                 chosenMove = this.getBeatingMove(this.getOpponentLastMove(gamestate));
             }else{
                 chosenMove = this.getRandomElement( ['R','P','S'] );
             }
+        }
+
+        if(gamestate.rounds.length > 1600){
+            this.dynamiteInterval = 3;
         }
 
         if (chosenMove === 'D') {
@@ -69,6 +102,25 @@ class Bot {
             }
         }
         return false;
+    }
+
+    private hasDrawnLastNRounds(gameState: Gamestate, n: number){
+        for (let i = gameState.rounds.length - 1; i >= (gameState.rounds.length - n) && i > 0; i--){
+            if (gameState.rounds[i].p2 !== gameState.rounds[i].p1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private numberOfPreviousConsecutiveDynamitedRounds(gameState: Gamestate, offset: number){
+        let i = gameState.rounds.length - 1 - offset;
+        let count = 0;
+        while(i > 0 && gameState.rounds[i].p2 === 'D'){
+            i--;
+            count++;
+        }
+        return count;
     }
 }
 
